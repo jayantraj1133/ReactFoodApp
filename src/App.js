150a8f1704dae70416dbc6560efc4f2f816849bd
jayantraj1133 <jayantraj1133@gmail.com>
2021-04-28 01:30:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,18 @@ const[search,setSearch]=useState("");
 const [query,setQuery]=useState('chicken')
 
 useEffect(()=>{
+  const getRecipies =async()=>{
+
+    const response =await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data= await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+    }
 getRecipies();
 
 },[query]);
 
-const getRecipies =async()=>{
 
-const response =await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-const data= await response.json();
-setRecipes(data.hits);
-console.log(data.hits);
-}
 
 const updateSearch=e=>{
   setSearch(e.target.value);
